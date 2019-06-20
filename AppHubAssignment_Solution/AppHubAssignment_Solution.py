5b709349fc845930605265f2abecaab7ac7c1841
DSBD <baciudavid.dragos@gmail.com>
2019-06-19 22:47:28 -0400

    Initial Project Setup & CoinFlip Program Complete & Tested

--- AppHubAssignment_Solution/AppHubAssignment_Solution.py
+++ AppHubAssignment_Solution/AppHubAssignment_Solution.py
@@ -1,1 +1,50 @@
+#Hatch Alpha AppHub Assignment Spring 2019 Solution
+#Coded By: Dragos Baciu-David
 
+import random
+
+def FibonacciGenerator(terms):
+    pass
+
+def PrimeFactorization(number):
+    pass
+
+def NextPrime(number):
+    pass
+
+def IsPrime(number):
+    pass
+
+def MortgageCalculator(loanValue, loanLength, interestRate):
+    pass
+
+def ChangeReturn(cost, tender):
+    pass
+
+def BinaryToDecimal(bNum):
+    pass
+
+def DecimalToBinary(dNum):
+    pass
+
+def UnitConverter():
+    pass
+
+def TaxCalulator(amount, taxRate):
+    pass
+
+def Factorial(number):
+    pass
+
+def CoinFlip():
+    result = random.randint(0, 1);
+    if(result == 0):
+        return "Heads"
+    elif(result == 1):
+        return "Tails"
+
+def main():
+    pass
+
+if __name__ == "__main__": #The following two lines of code run the main method apon program start as python does no do this by default
+    main()
